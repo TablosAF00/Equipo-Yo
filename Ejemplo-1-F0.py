717049112d2c7985caa1a98909667486478ac939
TablosAF00 <playstation2chipeada@gmail.com>
2025-03-03 23:04:32 -0600

    Create Ejemplo-1-F0.py
    Se coloco información sobre la finalidad del código de ejemplo así como el funcionamiento de cada uno de sus comandos.

---
+++ Ejemplo-1-F0.py
@@ -0,0 +1,25 @@
+#Equipo:Yo
+#Trabajo: Pensado Paredes Fredy Emanuel 
+# 2025/03/03 	V.1.0.2
+# El codigo tiene como proposito hacer las cuatro operaciones basicas con las variables que se establecieron
+numero1 = 5 #Se guarda el valor de la primer variable
+numero2 = 3 #Se guarda el segundo valor de la variable
+
+
+suma = numero1 + numero2 #Se rea una variable donde sera almacenado el resultado correspondiente a la suma
+resta = numero1 - numero2 #Se crea una variable donde se almacenara lel resultado de la opreacion resta 
+multiplicacion = numero1 * numero2 #Se crea otra variable donde se almacenara el resultado de la multuplicacion 
+division = numero1 / numero2 #Se crea una ultima variable donde al igual que las anteriores se almacenara el resultado de la division 
+
+
+print("Suma:", suma) #Se usa el comando print pata imprimir/mostrar el dato que esta almacenado en la variable suma
+print("Resta:", resta) #Se usa el comando print pata imprimir/mostrar el dato que esta almacenado en la variable resta
+print("Multiplicación:", multiplicacion) #Se usa el comando print pata imprimir/mostrar el dato que esta almacenado en la variable multiplicacion 
+print("División:", division) #Se usa el comando print pata imprimir/mostrar el dato que esta almacenado en la variable division 
+
+#Funcion de "print()"
+#Es utilizada para inprimit texto en pantalla, este debe ser colocado dentro de los parentesis para que la sintaxis sea correcta
+
+#Ejemplo
+#num1=9
+#print("num1",)
